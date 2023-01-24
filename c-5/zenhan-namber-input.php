8782536fc2252ada6ddf58d0563da59804f2e79b
yuusuuuke <jojo2nojojo@gmail.com>
2023-01-24 14:55:37 +0900

    fin: chap5-6

---
+++ c-5/zenhan-namber-input.php
@@ -0,0 +1,7 @@
+<?php require '../header.php'?>
+<p>購入個数を入力してください</p>
+<form action="zenhan-namber-output.php" method="post">
+  <input type="text" name="count">
+  <input type="submit" value="確定"z>
+</form>
+<?php require '../footer.php'?>
