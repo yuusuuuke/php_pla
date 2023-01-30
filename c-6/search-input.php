74a560a73e9934a6de72107a885ca4747bc3e0ad
yuusuuuke <jojo2nojojo@gmail.com>
2023-01-30 13:55:57 +0900

    name=? -> name like ?

---
+++ c-6/search-input.php
@@ -0,0 +1,8 @@
+<?php require '../header.php' ?>
+商品名を有力してください
+<form action="search-output.php" method="post">
+  <input type="text" name="keyword">
+  <input type="submit" value="検索">
+</form>
+
+<?php require '../footer.php' ?>
